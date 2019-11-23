4788c11a98cc5b485dc995748b7ef05a39391887
Justin Richer <justin@bspk.io>
2019-11-23 18:40:11 +0800

    refactored input panel

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -8,27 +8,27 @@ class MkJose extends React.Component {
 		
 		this.state = {
 			payload: '',
-			cibaPayload: {},
-			requestPayload: {},
 			alg: 'none',
 			key: {},
-			payloadMode: 'ciba'   // can also be 'ciba' or 'request'
+			payloadMode: 'ro'   // can also be 'ciba' or 'request'
 		};
 	}
 	
-	setPayload = (e) => {
-		if (this.state.mode == 'plain') {
-			this.setState({
-				payload: e.target.value
-			});
-		} 
+	setPayload = (val) => {
+		this.setState({
+			payload: val
+		});
+	}
+	
+	selectTab = (payloadMode) => () => {
+		this.setState({payloadMode: payloadMode});
 	}
 	
 	render() {
 		return (
 			<Section>
 				<Container>
-					<InputForm payload={this.state.payload} payloadMode={this.state.payloadMode} setPayload={this.setPayload} />
+					<InputForm payload={this.state.payload} payloadMode={this.state.payloadMode} setPayload={this.setPayload} selectTab={this.selectTab} />
 				</Container>
 				<Container>
 					<OutputForm />
@@ -40,10 +40,17 @@ class MkJose extends React.Component {
 
 const InputForm = ({...props}) => {
 	var label = 'Payload';
+	var payload = <PlainPayload payload={props.payload} 
+		setPayload={props.setPayload} />;
+
 	if (props.payloadMode == 'ro') {
 		label = 'Payload for Request Object';
+		payload = <RequestObjectPayload payload={props.payload} 
+			setPayload={props.setPayload} />;
 	} else if (props.payloadMode == 'ciba') {
 		label = 'Payload for Backchannel Authentication';
+		payload = <CibaPayload payload={props.payload} 
+			setPayload={props.setPayload} />;
 	}
 	return (
 		<div>
@@ -57,26 +64,24 @@ const InputForm = ({...props}) => {
 				</Level.Side>
 				<Level.Side align="right">
 					<Level.Item>
-						<Form.Field>
-							<Button color="light">Request Object</Button>
-						</Form.Field>
-					</Level.Item>
-					<Level.Item>
-						<Form.Field>
-							<Button color="light">CIBA</Button>
-						</Form.Field>
-					</Level.Item>
-					<Level.Item>
-						<Form.Field>
-							<Button color="light"><i className="far fa-trash-alt"></i></Button>
-						</Form.Field>
+						<Tabs type='boxed'>
+							<Tabs.Tab active={props.payloadMode == 'plain'} onClick={props.selectTab('plain')}>
+							Plain
+							</Tabs.Tab>
+							<Tabs.Tab active={props.payloadMode == 'ciba'} onClick={props.selectTab('ciba')}>
+							CIBA
+							</Tabs.Tab>
+							<Tabs.Tab active={props.payloadMode == 'ro'} onClick={props.selectTab('ro')}>
+							Request Object
+							</Tabs.Tab>
+						</Tabs>
 					</Level.Item>
 				</Level.Side>
 			</Level>
 			
 			<Form.Field>
 				<Form.Control>
-					<Payload payload={props.payload} mode={props.payloadMode} setPayload={props.setPayload} />
+					{payload}
 				</Form.Control>
 			</Form.Field>
 		
@@ -84,7 +89,225 @@ const InputForm = ({...props}) => {
 	);
 }
 
-const Payload = ({...props}) => {
+class PlainPayload extends React.Component {
+	setPayload = (e) => {
+		this.props.setPayload(e.target.value);
+	}
+	
+	render() {
+		return (
+			<Form.Textarea className="textarea" rows={10} spellCheck={false} 
+				value={this.props.payload} onChange={this.setPayload} />
+		);
+	}
+}
+
+class CibaPayload extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		var p = {};
+		try {
+			p = JSON.parse(props.payload);	
+		} catch (e) {
+			// non-json payload, ignore
+		}
+		
+		this.state = {
+			payload: p
+		};
+	}
+	
+	fieldTypes = {
+		exp: 'datetime-local',
+		nbf: 'datetime-local',
+		iss: 'text',
+		aud: 'text',
+		scope: 'text',
+		client_notification_token: 'text',
+		acr_values: 'text',
+		login_hint_token: 'text',
+		id_token_hint: 'text',
+		login_hint: 'text',
+		binding_message: 'text',
+		user_code: 'text',
+		requested_expiry: 'number',
+		request_context: 'text'
+	}
+
+	setPayloadItem = (field) => (e) => {
+		
+		var p = this.state.payload;
+		
+		var val = e.target.value;
+				
+		if (e.target.attributes["type"].value == 'number' 
+			|| e.target.attributes["type"].value == 'datetime-local') {
+			val = Number(val);
+		}
+		
+		p[field] = val;
+		
+		this.props.setPayload(JSON.stringify(p, null, 4));
+		
+		this.setState({
+			payload: p
+		});
+	}
+	
+	render() {
+		const fields = [];
+		Object.keys(this.fieldTypes).forEach((field) => {
+			fields.push(
+				<tr key={'ciba-' + field}>
+					<td><code>{field}</code></td>
+					<td><input id={'ciba-' + field} type={this.fieldTypes[field]} size={60} 
+						onChange={this.setPayloadItem(field)} value={this.state.payload[field] || ''} /></td>
+					<td><Button><i className="far fa-trash-alt"></i></Button></td>
+				</tr>
+			);
+		});
+		
+		return (
+			<Card color="light">
+				<Card.Content>
+					<Table>
+						<tbody>
+							{fields}
+						</tbody>
+					</Table>
+				</Card.Content>
+			</Card>
+		);
+	}
+}
+
+class RequestObjectPayload extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		var p = {};
+		try {
+			p = JSON.parse(props.payload);	
+		} catch (e) {
+			// non-json payload, ignore
+		}
+		
+		
+		var claims = '';
+		if (p.claims) {
+			claims = JSON.stringify(p.claims, null, 4);
+		}
+
+		console.log('Here');
+		
+		this.state = {
+			payload: p,
+			claimsString: claims
+		};
+	}
+
+	fieldTypes = {
+		exp: 'datetime-local',
+		iss: 'text',
+		aud: 'text',
+		scope: 'text',
+		response_type: 'text',
+		client_id: 'text',
+		redirect_uri: 'text',
+		state: 'text',
+		nonce: 'text',
+		max_age: 'number',
+		code_challenge: 'text',
+	}
+
+	setPayloadItem = (field) => (e) => {
+		
+		var p = this.state.payload;
+		
+		var val = e.target.value;
+				
+		if (e.target.attributes["type"].value == 'number' 
+			|| e.target.attributes["type"].value == 'datetime-local') {
+			val = Number(val);
+		}
+		
+		p[field] = val;
+		
+		this.props.setPayload(JSON.stringify(p, null, 4));
+		
+		this.setState({
+			payload: p
+		});
+	}
+	
+	setClaims = (e) => {
+		const val = e.target.value;
+
+		var p = this.state.payload;
+
+		if (!val) {
+			p['claims'] = c;
+
+			this.props.setPayload(JSON.stringify(p, null, 4));
+			
+		} else {
+		
+			// try to parse as json
+			var c = undefined;
+			try {
+				c = JSON.parse(val);	
+	
+				p['claims'] = c;
+	
+				this.props.setPayload(JSON.stringify(p, null, 4));
+				
+			} catch (e) {
+				// non-json payload, ignore the update
+			}
+		}
+		
+		this.setState({
+			payload: p,
+			claimsString: val
+		});
+		
+	}
+	
+	render() {
+		const fields = [];
+		Object.keys(this.fieldTypes).forEach((field) => {
+			fields.push(
+				<tr key={'ro-' + field}>
+					<td><code>{field}</code></td>
+					<td><input id={'ro-' + field} type={this.fieldTypes[field]} size={60} 
+						onChange={this.setPayloadItem(field)} value={this.state.payload[field] || ''} /></td>
+					<td><Button><i className="far fa-trash-alt"></i></Button></td>
+				</tr>
+			);
+		});
+		
+		return (
+			<Card color="light">
+				<Card.Content>
+					<Table>
+						<tbody>
+							{fields}
+							<tr>
+								<td><code>claims</code></td>
+								<td><textarea id="ro-claims" rows="5" cols="50" spellCheck="false"
+									onChange={this.setClaims} value={this.state.claimsString}></textarea></td>
+								<td><Button><i className="far fa-trash-alt"></i></Button></td>
+							</tr>
+						</tbody>
+					</Table>
+				</Card.Content>
+			</Card>
+		);
+	}
+}
+
+const PayloadInput = ({...props}) => {
 	if (props.mode == 'plain') {
 		return (
 			<Form.Textarea className="textarea" rows={10} spellCheck={false} value={props.payload} onChange={props.setPayload} />
@@ -92,44 +315,7 @@ const Payload = ({...props}) => {
 	} else if (props.mode == 'ro') {
 		return null;
 	} else if (props.mode == 'ciba') {
-		const fieldTypes = {
-			exp: 'datetime-local',
-			nbf: 'datetime-local',
-			iss: 'text',
-			aud: 'text',
-			scope: 'text',
-			client_notification_token: 'text',
-			acr_values: 'text',
-			login_hint_token: 'text',
-			id_token_hint: 'text',
-			login_hint: 'text',
-			binding_message: 'text',
-			user_code: 'text',
-			requested_expiry: 'number',
-			request_context: 'text'
-		};
-		const fields = [];
-		Object.keys(fieldTypes).forEach((field) => {
-			fields.push(
-				<tr key={'ciba-' + field}>
-					<td><code>{field}</code></td>
-					<td><input id={'ciba-' + field} type={fieldTypes[field]} size={60} /></td>
-					<td><Button><i className="far fa-trash-alt"></i></Button></td>
-				</tr>
-			);
-		});
-		
-		return (
-		<Card color="light">
-			<Card.Content>
-			<Table>
-				<tbody>
-					{fields}
-				</tbody>
-			</Table>
-			</Card.Content>
-		</Card>
-		);
+		return null
 	} else {
 		return null;
 	}
